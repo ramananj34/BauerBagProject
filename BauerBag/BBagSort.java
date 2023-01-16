c4624366f0430e0be77c16071beb4075110d5f97
Jude <juder.ramanan@gmail.com>
2023-01-15 19:46:04 -0500

    Merge Sort

--- BauerBag/BBagSort.java
+++ BauerBag/BBagSort.java
@@ -113,16 +113,59 @@ public final class BBagSort
         }
     }
 
+    /**
+     * Merge Sort
+     * This is a slightly more complicated sort that works by breaking down an array and then building it back up in order
+     * 
+     * @param arr is the array you are sorting
+     * @param startIndex is the lower bound of the range you are sorting
+     * @param endIndex is the upper bound of the range you are sorting
+     */
+    public static void mergeSort(int[] arr, int startIndex, int endIndex) 
+    {
+        int[] tempArr = new int[arr.length]; //To perform this, we need a temporary array so we don't overide data. You will see why
+        breakDown(arr,startIndex,endIndex,tempArr); //The first step is to break down the array. The recombination occurs recursivly
+    }
+
+    //Helper method to break down arrays for a merge sort
+    private static void breakDown(int[] arr, int startIndex, int endIndex, int[] tempArr)
+    {
+        if (startIndex >= endIndex) {return;} //If the indexes are equal, you know you have broken it down enough
+
+        breakDown(arr, startIndex, (startIndex+endIndex)/2, tempArr); //Break down the first half
+        breakDown(arr, 1+((startIndex+endIndex)/2), endIndex, tempArr); //Break down the second half
+        recombine(arr, startIndex, endIndex, tempArr); //Recombine the first and second half
+    }
+
+    //Helper method to reconstruct the array for a merge sort
+    private static void recombine(int[] arr, int startIndex, int endIndex, int[] tempArr)
+    {
+
+        int middle = ((startIndex + endIndex)/2); //Gets us the middle point, or where the halfs were
+        //How this works is by having two indexes, one for the first half, one for the second. As we go through the broken arrays, we increment these
+        int secondHalfIndex = middle+1; //Index for the first half
+        int firstHalfIndex = startIndex; //Index for the second half
+
+        for (int i = startIndex; i <= endIndex; i++) { //For each value we are recombining
+            if (firstHalfIndex>middle) //If the first half index passes the middle, we know it goes in the second half
+				tempArr[i] = arr[secondHalfIndex++]; //Put the value in the second half and move up that index
+			else if (secondHalfIndex > endIndex) //If the second half index passes the range, take the first half index
+				tempArr[i] = arr[firstHalfIndex++]; //Puts the value in the first half and moves on
+			else if (arr[firstHalfIndex] < arr[secondHalfIndex]) //If the value of the first half index is less than the second half, take the value from the first half
+				tempArr[i] = arr[firstHalfIndex++]; //Does above
+			else //If all else fails, use the second half index
+				tempArr[i] = arr[secondHalfIndex++]; //Uses the second half index
+        }
+
+        for (int i = startIndex; i <= endIndex; i++) { //Copy the values from the temp array back to the origional
+			arr[i] = tempArr[i];
+		}
+    }
+
     //This class contains private local methods that might be of use.
     private static final class Helpers
     {
-        /**
-         * Swap method swaps to value of the array
-         * 
-         * @param arr Is the array in question
-         * @param pos1 One value to swap
-         * @param pos2 Another value to swap
-         */
+        //Helper method to swap locations of an array
         private static void swap(int[] arr, int pos1, int pos2)
         {
             int temp = arr[pos1]; //Store the first value in a temp
